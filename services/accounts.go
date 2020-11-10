d6fd01863dfd7ab37d049aae1a7cdf5c16aef4ea
Mrucznik <mrucznix@gmail.com>
2020-11-11 00:05:03 +0100

    Accounts test data

--- services/accounts.go
+++ services/accounts.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"github.com/MruV-RP/mruv-api-placeholder/utils"
 	"github.com/MruV-RP/mruv-pb-go/accounts"
 )
 
@@ -13,21 +14,86 @@ func NewAccountsServer() *AccountsServer {
 }
 
 func (a *AccountsServer) RegisterAccount(ctx context.Context, request *accounts.RegisterAccountRequest) (*accounts.RegisterAccountResponse, error) {
-	panic("implement me")
+	if request.Login == "good" {
+		return &accounts.RegisterAccountResponse{
+			Success:   true,
+			AccountId: 1,
+		}, nil
+	} else if request.Login == "bad" {
+		return &accounts.RegisterAccountResponse{
+			Success:   false,
+			AccountId: 0,
+		}, nil
+	} else if request.Login == "error" {
+		return nil, utils.TestError
+	} else {
+		return nil, utils.UsageStringError
+	}
 }
 
 func (a *AccountsServer) LogIn(ctx context.Context, request *accounts.LogInRequest) (*accounts.LogInResponse, error) {
-	panic("implement me")
+	if request.Login == "good" {
+		return &accounts.LogInResponse{
+			Success:   true,
+			AccountId: 1,
+		}, nil
+	} else if request.Login == "bad" {
+		return &accounts.LogInResponse{
+			Success:   false,
+			AccountId: 0,
+		}, nil
+	} else if request.Login == "error" {
+		return nil, utils.TestError
+	} else {
+		return nil, utils.UsageStringError
+	}
 }
 
 func (a *AccountsServer) IsAccountExist(ctx context.Context, request *accounts.IsAccountExistRequest) (*accounts.IsAccountExistResponse, error) {
-	panic("implement me")
+	if request.Login == "good" {
+		return &accounts.IsAccountExistResponse{
+			Exists: true,
+			Id:     1,
+		}, nil
+	} else if request.Login == "bad" {
+		return &accounts.IsAccountExistResponse{
+			Exists: false,
+			Id:     0,
+		}, nil
+	} else if request.Login == "error" {
+		return nil, utils.TestError
+	} else {
+		return nil, utils.UsageStringError
+	}
 }
 
 func (a *AccountsServer) GetAccount(ctx context.Context, request *accounts.GetAccountRequest) (*accounts.GetAccountResponse, error) {
-	panic("implement me")
+	if request.Login == "good" {
+		return &accounts.GetAccountResponse{
+			Login: "TestLogin",
+			Email: "TestMail@gmail.com",
+		}, nil
+	} else if request.Login == "bad" {
+		return nil, utils.NotFoundError
+	} else if request.Login == "error" {
+		return nil, utils.TestError
+	} else {
+		return nil, utils.UsageStringError
+	}
 }
 
 func (a *AccountsServer) GetAccountCharacters(ctx context.Context, request *accounts.GetAccountCharactersRequest) (*accounts.GetAccountCharactersResponse, error) {
-	panic("implement me")
+	if request.Login == "good" {
+		return &accounts.GetAccountCharactersResponse{
+			CharacterIds: []uint32{1},
+		}, nil
+	} else if request.Login == "bad" {
+		return &accounts.GetAccountCharactersResponse{
+			CharacterIds: []uint32{},
+		}, nil
+	} else if request.Login == "error" {
+		return nil, utils.TestError
+	} else {
+		return nil, utils.UsageStringError
+	}
 }
